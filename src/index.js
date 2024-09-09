cf6b9867eda6af6a0ee6ca28b9d8342bda146006
David Singleton <davidjsingleton18@gmail.com>
2024-09-09 10:59:43 -0700

    Add unstyled but functional dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import './reset.css';
+
+const dropdownBtn = document.querySelector('#dropdown-button');
+const dropdownContainer = document.querySelector('#dropdown-container');
+
+dropdownBtn.addEventListener('click', () => {
+    dropdownContainer.classList.toggle('hidden');
+});
+
+dropdownContainer.addEventListener('click', (e) => {
+    console.log(e.target);
+});
